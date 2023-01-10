c2307f7f48fe6eb89b44d16d636f5744351adc78
Roman Shulga <96079947+romfantast@users.noreply.github.com>
2023-01-10 16:29:27 +0200

    fix path

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_KEY = 'ecf1e97b02d331ed89fab73e637afa61';
 
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL = 'http://api.themoviedb.org/3/';
 
 export const getTrending = () => {
   const data = axios.get(`${BASE_URL}trending/all/day?api_key=${API_KEY}`);
